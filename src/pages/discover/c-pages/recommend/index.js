b5c80990d8120201445e6d79df42ec73d96e9961
lizhiqiang17 <lizhiqiang17@58.com>
2022-07-14 15:50:56 +0800

    redux中hooks的使用

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,28 +1,49 @@
 import React, { memo, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { getTopBannersAction } from './store/actionCreators';
 
 function HYRecommend(props) {
-    const {getBanners, topBanners} = props;
-    useEffect(()=> {
-        getBanners();
-    }, [getBanners])
+    // 组件和redux关联：获取数据 + 进行操作
+    const {topBanners} = useSelector(state => ({
+        topBanners: state.recommend.topBanners
+    }));
+    const dispatch = useDispatch();
+
+    // 发送网络请求
+    useEffect(() => {
+        dispatch(getTopBannersAction());
+    }, [dispatch])
 
     return (
         <div>
-            HYRecommend：{topBanners.length}
+            HYRecommend:{topBanners.length}
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    topBanners: state.recommend.topBanners
-});
+export default memo(HYRecommend);
 
-const mapDispatchToProps = dispatch => ({
-    getBanners: () => {
-        dispatch(getTopBannersAction())
-    }
-})
+// function HYRecommend(props) {
+//     const {getBanners, topBanners} = props;
+//     useEffect(()=> {
+//         getBanners();
+//     }, [getBanners])
 
-export default connect(mapStateToProps, mapDispatchToProps)(memo(HYRecommend));
+//     return (
+//         <div>
+//             HYRecommend：{topBanners.length}
+//         </div>
+//     )
+// }
+
+// const mapStateToProps = state => ({
+//     topBanners: state.recommend.topBanners
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//     getBanners: () => {
+//         dispatch(getTopBannersAction())
+//     }
+// })
+
+// export default connect(mapStateToProps, mapDispatchToProps)(memo(HYRecommend));
